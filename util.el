da5d52b0d28aa04887d7d6b30563c7e75b0b0cf8
Simen Heggestøyl <simenheg@runbox.com>
2021-12-07 17:54:51 +0100

    7-1 done

--- util.el
+++ util.el
@@ -6,5 +6,14 @@
     (insert-file-contents file)
     (split-string (string-trim (buffer-string)) "\n")))
 
+(defun slurp-numbers (file)
+  (with-temp-buffer
+    (insert-file-contents file)
+    (mapcar #'string-to-number
+            (split-string (string-trim (buffer-string)) ","))))
+
 (defun transpose (matrix)
   (apply #'seq-mapn (lambda (&rest x) x) matrix))
+
+(defun sum (list)
+  (cl-reduce #'+ list))
